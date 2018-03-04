94c4b2cea21fbbbcfa62f5e47d2e9fad3402d8f1
KaileyCozart <kaileync@comcast.net>
2018-03-04 01:59:58 -0800

    Possible Fade-Ins...

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-window.onload = function() {
 
+
+window.onload = function() {
+    
+     // Fade In Divs
+     document.getElementById("fade").className += " appear";
+
+    // Check That Passwords Are the Same
+    /*
     var PW1 = document.getElementById("pw1");
     var PW2 = document.getElementById("pw2");
     var warning = document.getElementById("passwordWarning");
@@ -13,5 +20,6 @@ window.onload = function() {
             warning.className = "disappearWarning";
         }
     } 
+    */
 
 };
